1a581c9b9751e015cecd4e6fa5e1bc0e58716700
Edward M murray Jr <ampedej@fullsail.edu>
2013-01-09 22:29:50 -0500

    Added getData function. Can't get it to work correctly.

--- js/main.js
+++ js/main.js
@@ -6,10 +6,6 @@ ASD 1301
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
 });
-
-/*var storeData = function(data){
-	console.log(data);
-};*/
 	
 $('#additem').on('pageinit', function(){
 
@@ -34,12 +30,13 @@ $('#additem').on('pageinit', function(){
 	}
 	
 	function storeData(key){
+
 		var id					= Math.floor(Math.random()*100000001);
+		
 		getSelectedRadio()
 		var item				= {};
 			item.rname 			= ["Recipe Name:", $('#rname').val()];
 			item.dateadded 		= ["Date Added:", $('#dateadded').val()];
-			item.rating 		= ["Rating:", $('#rating').val()];
 			item.category 		= ["Category:", categoryValue];
 			item.rtype 			= ["Recipe Type:", $('#rtype').val()];
 			item.ringredients 	= ["Recipe Ingredients:", $('#ringredients').val()];
@@ -48,8 +45,39 @@ $('#additem').on('pageinit', function(){
 		//Save into local storage
 		localStorage.setItem(id, JSON.stringify(item));
 		alert("Recipe Saved!");
+		$("#recipeform").resetForm();
 	}
 	
+	function getData(){
+		if(localStorage.length === 0){
+			alert("There are no recipes stored. Default recipes were added.");
+		}
+		var makeDiv = $('<div></div>');
+		$('#makeDiv').attr("id", "items");
+		var makeList = $('ul');
+		$('#makeDiv').append('makeList');
+		$('document.body').append('makeDiv');
+		$('items').show();
+		for(var i=0, length=localStorage.length; i<length; i++){
+			var makeLi = $('li');
+			var linksLi = $('li');
+			$('makeList').append('makeLi');
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var obj = JSON.parse(value);
+			var makeSubList = $('ul');
+			$('makeLi').append('makeSubList');
+			for (var n in obj){
+				var makeSubLi = $('li');
+				$('makeSubList').append('makeSubLi');
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+				$('makeSubList').append('linksLi');
+			}
+		}
+	}
+	var displayLink = $('displayLink');
+	$('#displayLink').on("click", getData);
 	
 });
 
